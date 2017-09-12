75dd9a12a993cc2ab48b83869b96c2023c36d303
Kasidit Iamthong <kasidit@outlook.com>
2017-09-12 10:34:55 +0700

    Separate homepage into HomeModule

--- frontend-webapp/src/app/home/home-routing.module.ts
+++ frontend-webapp/src/app/home/home-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home.component';
+
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    loadChildren: './home/home.module#HomeModule'
+    component: HomeComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
