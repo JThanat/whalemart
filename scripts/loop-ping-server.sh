a51578885987a5a33f90977997fcc62582c91f6d
Kasidit Iamthong <kasidit@outlook.com>
2017-09-14 00:43:15 +0700

    Add echo to loop-ping-server

--- scripts/loop-ping-server.sh
+++ scripts/loop-ping-server.sh
@@ -4,4 +4,5 @@ while [ "$PING_RETURN_CODE" != "200" ]
 do
   sleep 1
   PING_RETURN_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/ping/)
+  echo "Ping Return Code: $PING_RETURN_CODE"
 done
