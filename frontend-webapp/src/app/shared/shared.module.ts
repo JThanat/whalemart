f7ab24eaa2dc089b0368028953ff4022fdef12fd
Kasidit Iamthong <kasidit@outlook.com>
2017-10-08 01:18:41 +0700

    Add ReactiveFormsModule

--- frontend-webapp/src/app/shared/shared.module.ts
+++ frontend-webapp/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   declarations: [],
   exports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
