87f0dff26a281f7995b6f1a88f6529deff5b95da
Kasidit Iamthong <kasidit@outlook.com>
2017-11-05 02:25:15 +0700

    Fix tests

--- frontend-webapp/src/app/app.component.spec.ts
+++ frontend-webapp/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -12,6 +13,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
--- frontend-webapp/src/app/core/nav-bar/nav-bar.component.spec.ts
+++ frontend-webapp/src/app/core/nav-bar/nav-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavBarComponent } from './nav-bar.component';
 
@@ -8,9 +9,9 @@ describe('NavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [NavBarComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
