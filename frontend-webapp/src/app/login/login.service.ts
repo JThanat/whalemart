82f91bb01176cd685fc089813a558e8e92d26abc
Kasidit Iamthong <kasidit@outlook.com>
2017-11-10 09:45:13 +0700

    Use real API for login

--- frontend-webapp/src/app/login/login.service.ts
+++ frontend-webapp/src/app/login/login.service.ts
@@ -7,14 +7,13 @@ import { catchError, map } from 'rxjs/operators';
 import { UserInfo, UserService } from '../core/user/user.service';
 
 export interface LoginServerResponse {
-  success: true;
+  token: string;
 }
 
 export type LoginErrorReason = 'INVALID_EMAIL_PASSWORD';
 
 export interface LoginServerResponseError {
-  success: false;
-  reason: LoginErrorReason;
+  non_field_errors: LoginErrorReason[];
 }
 
 export class LoginError {
@@ -30,7 +29,12 @@ export class LoginService {
   constructor(private http: HttpClient, private userService: UserService) { }
 
   login(email: string, password: string): Observable<UserInfo> {
-    return this.http.post<LoginServerResponse>('/api/login/', { email, password }).pipe(
+    return this.http
+      .post<LoginServerResponse>('/api/api-token-auth/', {
+        username: email,
+        password
+      })
+      .pipe(
       map(result => {
         const userInfo: UserInfo = { email };
         this.userService.userInfo.next(userInfo);
@@ -39,8 +43,8 @@ export class LoginService {
       catchError(err => {
         if (err instanceof HttpErrorResponse) {
           const errorBody = err.error as LoginServerResponseError;
-          if (errorBody.success === false) {
-            return observableThrow(new LoginError(errorBody.reason));
+          if (Array.isArray(errorBody.non_field_errors)) {
+            return observableThrow(new LoginError('INVALID_EMAIL_PASSWORD'));
           }
         }
         return observableThrow(err);
