468943661cdc998a211c6e53e32dde28af33a4c3
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-11-11 01:06:15 +0700

    Add accessories model

---
+++ webserver/apps/accessories/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AccessoriesConfig(AppConfig):
+    name = 'accessories'
--- webserver/config/settings.py
+++ webserver/config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.users',
     'apps.markets',
+    'apps.accessories',
 
     'django.contrib.admin',
     'django.contrib.auth',
