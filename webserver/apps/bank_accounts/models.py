9037980707fdf03f2108f3158b293c1bdc404e09
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-13 01:25:13 +0700

    :wrench: Add verbose_name

--- webserver/apps/bank_accounts/models.py
+++ webserver/apps/bank_accounts/models.py
@@ -3,14 +3,15 @@ from apps.lessors.models import Lessor
 
 # Create your models here.
 class BankAccount(models.Model):
-    account_name = models.CharField(max_length=30)
-    account_id = models.CharField(max_length=20, unique=True)
-    bank = models.CharField(max_length=5)
-    branch = models.CharField(max_length=20)
+    account_name = models.CharField(verbose_name='Account Name', max_length=30)
+    account_id = models.CharField(verbose_name='Account ID', max_length=20, unique=True)
+    bank = models.CharField(verbose_name='Bank', max_length=5)
+    branch = models.CharField(verbose_name='Branch', max_length=20)
 
     lessor = models.ForeignKey(
         Lessor,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='Lessor',
     )
 
     def __str__(self):
