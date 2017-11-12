cf1a2b3f67654a6bfb97068c9a60ceb21adc542c
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-12 21:14:08 +0700

    Update user fields

--- webserver/apps/users/models.py
+++ webserver/apps/users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     # Required for registration
-    email = models.EmailField(verbose_name='Email')
+    email = models.EmailField(verbose_name='Email', unique=True)
     first_name = models.CharField(verbose_name='First Name', max_length=50)
     last_name = models.CharField(verbose_name='Last Name', max_length=50)
     phone = models.CharField(verbose_name='Phone', max_length=20)
-    facebook_token = models.CharField(verbose_name='Facebook Token', max_length=255, blank=True)
+    facebook_token = models.TextField(verbose_name='Facebook Token', max_length=255, blank=True)
 
     # Vendor profile
     def __str__(self):
