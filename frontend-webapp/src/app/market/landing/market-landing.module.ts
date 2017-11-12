efc44b55f4831b984e05bc9eba63fa65e813b1cf
Kasidit Iamthong <kasidit@outlook.com>
2017-11-12 23:40:24 +0700

    Use SharedModule instead

--- frontend-webapp/src/app/market/landing/market-landing.module.ts
+++ frontend-webapp/src/app/market/landing/market-landing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
 
 import { MarketLandingRoutingModule } from './market-landing-routing.module';
 import { MarketLandingComponent } from './market-landing.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     MarketLandingRoutingModule
   ],
   declarations: [MarketLandingComponent]
