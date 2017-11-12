a360a5fc6aad0e44c81b8733d36b11e7324592e1
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-13 03:52:23 +0700

    Update serializer

--- webserver/apps/users/serializers.py
+++ webserver/apps/users/serializers.py
@@ -8,12 +8,15 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'phone', 'facebook_token')
+        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'phone', 'facebook_token', 'is_verified',
+                  'profile_image')
         extra_kwargs = {
-            'password': {'write_only': True}
+            'password': {'write_only': True},
+            'is_verified': {'read_only': True}
         }
 
     def validate(self, data):
+        data = super(UserSerializer, self).validate(data)
         data['username'] = data['email']
         data['password'] = make_password(data['password'])
         return data
