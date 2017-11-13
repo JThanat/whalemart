628ab27b4bd09b04bd2c243947b89b83608237d1
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 03:53:24 +0700

    Complete update part

--- webserver/apps/users/serializers.py
+++ webserver/apps/users/serializers.py
@@ -41,7 +41,11 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         credit_cards_data = validated_data.pop('credit_cards', None)
         instance = super(UserSerializer, self).update(instance, validated_data)
+
+        current_credit_cards = CreditCard.objects.filter(user=instance.id)
+        validated_credit_cards = []
         for credit_card_data in credit_cards_data:
+            # Update existing credit card
             credit_card_id = credit_card_data.get('id', None)
             if credit_card_id:
                 credit_card_obj = CreditCard.objects.get(id=credit_card_id)
@@ -50,8 +54,14 @@ class UserSerializer(serializers.ModelSerializer):
                                                                         credit_card_obj.card_holder_name)
                 credit_card_obj.type = credit_card_data.get('type', credit_card_obj.type)
                 credit_card_obj.expiry_date = credit_card_data.get('expiry_date', credit_card_obj.expiry_date)
-                credit_card_obj.verification_no = credit_card_data.get('verification_no', credit_card_obj.verification_no)
+                credit_card_obj.verification_no = credit_card_data.get('verification_no',
+                                                                       credit_card_obj.verification_no)
                 credit_card_obj.save()
+            # Create new credit card
             else:
                 CreditCard.objects.create(user=instance, **credit_card_data)
+        # Delete credit card deleted by frontend
+        to_delete_credit_cards = set(current_credit_cards) - set(validated_credit_cards)
+        for credit_card_obj in to_delete_credit_cards:
+            credit_card_obj.delete()
         return instance
