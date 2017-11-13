5b8d8cb520d0451d8e4a2136333eeea2f3632564
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 02:55:01 +0700

    Remove credit card app

--- webserver/apps/credit_cards/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CreditCardsConfig(AppConfig):
-    name = 'apps.credit_cards'
--- webserver/apps/credit_cards/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-
-from .serializers import CreditCardSerializer
-from .models import CreditCard
-
-
-
