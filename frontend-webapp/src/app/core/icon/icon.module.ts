28711ae55517fd82120c7f301e423709288eb655
Kasidit Iamthong <kasidit@outlook.com>
2017-11-14 03:39:51 +0700

    Provide SVG Icon definitions

--- frontend-webapp/src/app/core/icon/icon.module.ts
+++ frontend-webapp/src/app/core/icon/icon.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { SharedModule } from '../../shared/shared.module';
-import { SvgIconRegistry } from './svg-icon-registry.service';
+import { SVG_ICON_DEFINITIONS, SvgIconRegistry } from './svg-icon-registry.service';
+
+const svgIconNames: string[] = [];
 
 @NgModule({
   imports: [
     SharedModule
   ],
   providers: [
-    SvgIconRegistry
+    SvgIconRegistry,
+    {
+      provide: SVG_ICON_DEFINITIONS,
+      useFactory: (domSanitizer: DomSanitizer) => svgIconNames.map(svgIconName => ({
+        key: svgIconName,
+        fileUrl: domSanitizer.bypassSecurityTrustResourceUrl(`/assets/icon/${svgIconName}.svg`)
+      }))
+    }
   ]
 })
 export class IconModule { }
