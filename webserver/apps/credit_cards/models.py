1d501fc19827436318c0c5b47aa6873c4e08fada
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 02:53:22 +0700

    Move credit card model to user model

--- webserver/apps/credit_cards/models.py
+++ webserver/apps/credit_cards/models.py
@@ -1,18 +1,4 @@
 from django.db import models
 
 
-class CreditCard(models.Model):
-    card_number = models.CharField(verbose_name='Card Number', max_length=20)
-    card_holder_name = models.CharField(verbose_name='Card Holder Name', max_length=50)
-    MASTER_CARD = 1
-    VISA = 2
-    TYPE_CHOICES = (
-        (MASTER_CARD, 'MasterCard'),
-        (VISA, 'Visa')
-    )
-    type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES)
-    expiry_date = models.DateField(verbose_name='Expiry Date')
-    verification_no = models.CharField(verbose_name='Verification Number', max_length=10)
 
-    def __str__(self):
-        return self.card_number
--- webserver/apps/users/models.py
+++ webserver/apps/users/models.py
@@ -13,3 +13,21 @@ class User(AbstractUser):
     # Vendor profile
     def __str__(self):
         return '%s' % self.email
+
+
+class CreditCard(models.Model):
+    user = models.ForeignKey('user.User', verbose_name='User')
+    card_number = models.CharField(verbose_name='Card Number', max_length=20)
+    card_holder_name = models.CharField(verbose_name='Card Holder Name', max_length=50)
+    MASTER_CARD = 1
+    VISA = 2
+    TYPE_CHOICES = (
+        (MASTER_CARD, 'MasterCard'),
+        (VISA, 'Visa')
+    )
+    type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES)
+    expiry_date = models.DateField(verbose_name='Expiry Date')
+    verification_no = models.CharField(verbose_name='Verification Number', max_length=10)
+
+    def __str__(self):
+        return self.card_number
