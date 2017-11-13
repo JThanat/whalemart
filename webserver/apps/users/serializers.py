07c3b05e1d269a79de0070eee8a7efa25624fff3
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 04:51:54 +0700

    Fix bug

--- webserver/apps/users/serializers.py
+++ webserver/apps/users/serializers.py
@@ -21,18 +21,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'phone', 'facebook_token')
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'facebook_token', 'credit_cards')
+        # extra_kwargs = {
+        #     'password': {'write_only': True}
+        # }
 
-    def validate(self, data):
-        data['username'] = data['email']
-        data['password'] = make_password(data['password'])
-        return data
+    # def validate(self, data):
+        # data['username'] = data['email']
+        # data['password'] = make_password(data['password'])
+        # return data
 
     def create(self, validated_data):
         credit_cards_data = validated_data.pop('credit_cards', None)
+        validated_data['username'] = validated_data['email']
+        validated_data['password'] = make_password(validated_data['password'])
         user = User.objects.create(**validated_data)
         for credit_card_data in credit_cards_data:
             CreditCard.objects.create(user=user, **credit_card_data)
@@ -49,6 +51,7 @@ class UserSerializer(serializers.ModelSerializer):
             credit_card_id = credit_card_data.get('id', None)
             if credit_card_id:
                 credit_card_obj = CreditCard.objects.get(id=credit_card_id)
+                credit_card_obj.user = instance
                 credit_card_obj.card_number = credit_card_data.get('card_number', credit_card_obj.card_number)
                 credit_card_obj.card_holder_name = credit_card_data.get('card_holder_name',
                                                                         credit_card_obj.card_holder_name)
@@ -59,7 +62,8 @@ class UserSerializer(serializers.ModelSerializer):
                 credit_card_obj.save()
             # Create new credit card
             else:
-                CreditCard.objects.create(user=instance, **credit_card_data)
+                credit_card_obj = CreditCard.objects.create(user=instance, **credit_card_data)
+                validated_credit_cards.append(credit_card_obj)
         # Delete credit card deleted by frontend
         to_delete_credit_cards = set(current_credit_cards) - set(validated_credit_cards)
         for credit_card_obj in to_delete_credit_cards:
