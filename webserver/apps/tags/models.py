0293fab4105c41887157424ecfd411da137868dc
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-11-14 02:42:27 +0700

    Add Tag Model

--- webserver/apps/tags/models.py
+++ webserver/apps/tags/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    market = models.ManyToManyField('markets.Market')
+    tag = models.CharField(verbose_name='Tag', max_length=255)
