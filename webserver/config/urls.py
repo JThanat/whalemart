cce86a036e9a1aae24a2cb122ae78893a4164a35
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-11-14 02:49:52 +0700

    Reformat url page

--- webserver/config/urls.py
+++ webserver/config/urls.py
@@ -23,22 +23,24 @@ from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
+from apps.markets import views as market_views
 from apps.ping import views as ping_views
 from apps.users import views as user_views
 
 router = routers.DefaultRouter()
 router.register(r'users', user_views.UserViewSet)
+router.register(r'markets', market_views.MarketViewSet, base_name='markets')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^ping/', ping_views.Ping.as_view()),
-    url(r'^validate-email/', user_views.ValidateUserEmailView.as_view()),
-    url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # JWT
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(r'^', include(router.urls)),
+                  url(r'^ping/', ping_views.Ping.as_view()),
+                  url(r'^validate-email/', user_views.ValidateUserEmailView.as_view()),
+                  url(r'^admin/', admin.site.urls),
+                  url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                  # JWT
+                  url(r'^api-token-auth/', obtain_jwt_token),
+                  url(r'^api-token-refresh/', refresh_jwt_token),
+                  url(r'^api-token-verify/', verify_jwt_token),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
