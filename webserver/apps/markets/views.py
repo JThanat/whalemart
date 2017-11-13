506ed7bb774489991a169c3dfbf3f58d401001ea
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-11-14 02:41:56 +0700

    Add Market View

--- webserver/apps/markets/views.py
+++ webserver/apps/markets/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.markets.models import Market
+from apps.markets.serializers import MarketSerializer, MarketFeedSerializer
+
+
+class MarketViewSet(viewsets.ViewSet):
+    """
+    A ViewSet for listing market
+    """
+    def list(self, request):
+        queryset = Market.objects.all()
+        serializer = MarketFeedSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+
+        queryset = Market.objects.all()
+        market = get_object_or_404(queryset, pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+
