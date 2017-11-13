dbb2fc97e67480b8031f6a725b259c1e7c463644
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 02:41:59 +0700

    Clear unused import

--- webserver/apps/users/views.py
+++ webserver/apps/users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.hashers import check_password
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
