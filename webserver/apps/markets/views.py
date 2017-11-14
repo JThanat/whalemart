73d79273956cc89380c25a2c36b4b291bcada9e3
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-14 08:22:15 +0700

    Add filter and search function

--- webserver/apps/markets/views.py
+++ webserver/apps/markets/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework import filters
 
 from apps.markets.models import Market
 from apps.markets.serializers import MarketSerializer, MarketFeedSerializer
@@ -37,6 +38,8 @@ class MarketViewSet(viewsets.ModelViewSet):
 class MarketFeedViewSet(viewsets.GenericViewSet):
     serializer_class = MarketFeedSerializer
     queryset = Market.objects.all().order_by('-created_at')
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'location')
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
@@ -48,3 +51,30 @@ class MarketFeedViewSet(viewsets.GenericViewSet):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def filter_queryset(self, queryset):
+        query_params = self.request.query_params
+        min_price = query_params.get('min-price', None)
+        max_price = query_params.get('max-price', None)
+        opening_time = query_params.get('opening-time', None)
+        min_opening_date = query_params.get('min-opening-date', None)
+        max_opening_date = query_params.get('max-opening-date', None)
+        sort_by = query_params.get('sort-by', None)
+
+        if min_price:
+            queryset = queryset.exclude(max_price_lt=min_price)
+        if max_price:
+            queryset = queryset.exclude(min_price_gt=max_price)
+        if min_opening_date:
+            queryset = queryset.exclude(closing_date_lt=min_opening_date)
+        if max_opening_date:
+            queryset = queryset.exclude(opening_date_gt=max_opening_date)
+        if sort_by:
+            queryset = queryset.sort_by(sort_by)
+        # if date:
+        #     if not product or not master_contract:
+        #         raise serializers.ValidationError(_('Please provide product and master_contract'))
+        #     queryset = queryset.filter(start_date__lte=date).order_by('-start_date')
+        #     if len(queryset) != 0:
+        #         queryset = [queryset[0],]
+        return queryset
