f47033c2fcdb67deebca5ea3292fc326d95b28a7
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-14 23:09:09 +0700

    Add exit 1 when ping exceed 30

--- scripts/loop-ping-server.sh
+++ scripts/loop-ping-server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 counter=1
+max_ping=30
+
 echo "Begin"
-while [ "$PING_RETURN_CODE" != "200" ] && [ $counter -le 30 ]
+while [ "$PING_RETURN_CODE" != "200" ] && [ $counter -le $max_ping ]
 do
   sleep 1
   PING_RETURN_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/ping/)
   echo "$counter: Ping Return Code: $PING_RETURN_CODE"
   ((counter++))
 done
+if [ $counter -gt $max_ping ]; then
+  exit 1
+fi
