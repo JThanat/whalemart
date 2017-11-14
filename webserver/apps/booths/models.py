8350fc5115d9dd641525a9994ad5787ffdad7a81
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-14 19:38:46 +0700

    Add booth model

---
+++ webserver/apps/booths/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from apps.users.models import User
+
+
+class Booth(models.Model):
+    booth_number = models.CharField(verbose_name="Booth Number", max_length=5)
+    rental_free = models.IntegerField(verbose_name="Rental Free")
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='User',
+    )
+
+    def __str__(self):
+        return self.booth_number
---
+++ webserver/apps/booths/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Booth
+
+
+class BoothSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booth
+        fields = ('booth_number', 'rental_free', 'user')
