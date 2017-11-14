8903cb5924270f66fada62ac2cf689e85115b0e5
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-14 22:10:57 +0700

    Test loop ping on travis

--- scripts/loop-ping-server.sh
+++ scripts/loop-ping-server.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-while [ "$PING_RETURN_CODE" != "200" ]
+counter=1
+echo "Begin"
+while [ "$PING_RETURN_CODE" != "200" ] && [ $counter -le 30 ]
 do
   sleep 1
   PING_RETURN_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8000/ping/)
-  echo "Ping Return Code: $PING_RETURN_CODE"
+  echo "$counter: Ping Return Code: $PING_RETURN_CODE"
+  ((counter++))
 done
