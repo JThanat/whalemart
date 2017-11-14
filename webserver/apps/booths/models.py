2819934aa9c94f5d4491ed5b4bf3de9cc59acc90
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-14 21:12:37 +0700

    Fix typo again T_T

--- webserver/apps/booths/models.py
+++ webserver/apps/booths/models.py
@@ -4,7 +4,7 @@ from apps.users.models import User
 
 class Booth(models.Model):
     booth_number = models.CharField(verbose_name="Booth Number", max_length=5)
-    rental_fee = models.IntegerField(verbose_name="Rental Free")
+    rental_fee = models.IntegerField(verbose_name="Rental Fee")
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
