e0766a1882e4647238183f7b46da51cc531894ea
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-16 06:30:43 +0700

    Complete pagination

--- frontend-webapp/src/app/shared/pagination/pagination.component.ts
+++ frontend-webapp/src/app/shared/pagination/pagination.component.ts
@@ -1,16 +1,59 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
+  public numCenter: number[];
+  public showLeft: boolean;
+  public showDotLeft: boolean;
+  public showRight: boolean;
+  public showDotRight: boolean;
 
-  constructor() { }
+  @Input() min: number;
+  @Input() max: number;
+  @Input() current: number;
+  @Input() padding = 1;
 
-  ngOnInit() {
+  updateParams() {
+    this.min = +this.min;
+    this.max = +this.max;
+    this.current = +this.current;
+
+    if (this.current < this.min) {
+      this.current = this.min;
+    } else if (this.current > this.max) {
+      this.current = this.max;
+    }
+
+    this.showLeft = (this.current - this.min > this.padding);
+    this.showRight = (this.max - this.current > this.padding);
+
+    this.showDotLeft = (this.current - this.min > this.padding + 1);
+    this.showDotRight = (this.max - this.current > this.padding + 1); 
+
+    const centerLeft = Math.max(
+      Math.min(this.current - this.padding, this.max - this.padding * 2),
+      this.min
+    );
+    const centerRight = Math.min(
+      Math.max(this.current + this.padding, this.min + this.padding * 2),
+      this.max
+    );
+
+    this.numCenter = [];
+    for (let i = centerLeft; i <= centerRight; i++) {
+      this.numCenter.push(i);
+    }
   }
 
+  ngOnInit() {
+    this.updateParams();
+  }
+
+  ngOnChanges() {
+    this.updateParams();
+  }
 }
--- frontend-webapp/src/app/shared/shared.module.ts
+++ frontend-webapp/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { InputErrorComponent } from './input/input-error.component';
 import { InputGroupComponent } from './input/input-group.component';
 import { InputDirective } from './input/input.directive';
+import { PaginationComponent } from './pagination/pagination.component';
 import { SvgIconComponent } from './svg-icon/svg-icon.component';
 
 @NgModule({
@@ -24,7 +25,8 @@ import { SvgIconComponent } from './svg-icon/svg-icon.component';
     InputGroupComponent,
     InputDirective,
     InputErrorComponent,
-    SvgIconComponent
+    SvgIconComponent,
+    PaginationComponent
   ],
   exports: [
     CommonModule,
@@ -34,7 +36,8 @@ import { SvgIconComponent } from './svg-icon/svg-icon.component';
     InputGroupComponent,
     InputDirective,
     InputErrorComponent,
-    SvgIconComponent
+    SvgIconComponent,
+    PaginationComponent
   ]
 })
 export class SharedModule { }
