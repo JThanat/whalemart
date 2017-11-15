4ec7e7ebf5d3468ffb912df64061905015cdbe4f
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-16 03:50:23 +0700

    Fix linter T_______________T

--- frontend-webapp/src/app/shared/market/market.component.spec.ts
+++ frontend-webapp/src/app/shared/market/market.component.spec.ts
@@ -1,5 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MarketComponent } from './market.component';
 
