52ccb04d3d2541d841de9ee1dd5d115ce2e92138
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-16 00:46:11 +0700

    Fix booth model

--- webserver/apps/booths/models.py
+++ webserver/apps/booths/models.py
@@ -4,7 +4,8 @@ from apps.markets.models import Market
 
 class Booth(models.Model):
     booth_number = models.CharField(verbose_name="Booth Number", max_length=5)
-    rental_fee = models.IntegerField(verbose_name="Rental Fee")
+    rental_fee = models.DecimalField(
+        verbose_name="Rental Fee", max_digits=10, decimal_places=2)
     market = models.ForeignKey(
         Market,
         on_delete=models.CASCADE,
@@ -12,7 +13,7 @@ class Booth(models.Model):
     )
 
     class Meta:
-        unique_together = ('market', 'booth_number')
+        unique_together = (('market', 'booth_number'),)
 
     def __str__(self):
-        return self.booth_number
+        return '%s - %s' % (self.market.name, self.booth_number)
