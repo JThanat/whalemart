d58493b19e9407c16ffebbf832421a6cb32e07eb
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-15 23:32:52 +0700

    Toggle form control (not finished)

--- frontend-webapp/src/app/become-lessor/become-lessor.component.ts
+++ frontend-webapp/src/app/become-lessor/become-lessor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+// import { BecomeLessorService } from './become-lessor.service'
 // import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +10,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class BecomeLessorComponent implements OnInit {
   becomeLessorForm: FormGroup;
+  isOrganization: Boolean;
 
   constructor(
     // private router: Router
@@ -18,14 +20,50 @@ export class BecomeLessorComponent implements OnInit {
     this.becomeLessorForm = new FormGroup({
       lessorName: new FormControl('', [Validators.required]),
       isOrganization: new FormControl('', [Validators.required]),
-      organizationName: new FormControl(''),
-      organizationContactName: new FormControl(''),
-      organizationEmail: new FormControl(''),
-      organizationPhone: new FormControl(''),
+      organizationName: new FormControl('', [Validators.required]),
+      organizationContactName: new FormControl('', [Validators.required]),
+      organizationEmail: new FormControl('', [Validators.required]),
+      organizationPhone: new FormControl('', [Validators.required]),
     }, { updateOn: 'blur' });
+
+    this.isOrganization = false;
+    this.setValidateOnOrganization(this.isOrganization);
+  }
+
+  toggleIsOrganization() {
+    this.isOrganization = !this.isOrganization;
+    this.setValidateOnOrganization(this.isOrganization);
+  }
+
+  setValidateOnOrganization(isEnable: Boolean) {
+    const fields: string[] = [
+      'organizationName',
+      'organizationContactName',
+      'organizationEmail',
+      'organizationPhone'
+    ];
+
+    for (const field in fields) {
+      if (isEnable) {
+        this.becomeLessorForm.controls[field].enable();
+      } else {
+        this.becomeLessorForm.controls[field].disable();
+      }
+    }
   }
 
   becomeLessor() {
     this.becomeLessorForm.updateValueAndValidity();
+
+    if (!this.becomeLessorForm.valid) {
+      return ;
+    }
+
+    // const { 
+    //   lessorName, isOrganization, organizationName, organizationContactName,
+    //   organizationEmail, organizationPhone      
+    // } = this.becomeLessorForm.value;
+
+    
   }
 }
