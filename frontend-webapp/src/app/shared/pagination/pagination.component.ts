d908ffab42a6f46cfb0d9012b211285728394405
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-16 04:37:33 +0700

    Add pagination setup

---
+++ frontend-webapp/src/app/shared/pagination/pagination.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
