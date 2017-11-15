1898ec8ab9aa712bd9cb8cec235e60d807fa9252
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-16 04:42:21 +0700

    Fix currency

--- frontend-webapp/src/app/shared/market/market.component.ts
+++ frontend-webapp/src/app/shared/market/market.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 export interface Market {
   expireDay: number;
   imageURL: string;
-  marketName: string;
+  name: string;
   location: string;
   startDate: Date;
   endDate: Date;
@@ -19,7 +19,7 @@ export class MarketComponent {
   @Input() market: Market = {
     imageURL: 'https://www.petful.com/wp-content/uploads/2013/04/160490011_b0cecf8fa1_z.jpg',
     expireDay: 3,
-    marketName: 'ตลาดอิอิ',
+    name: 'ตลาดอิอิ',
     startDate: new Date(),
     endDate: new Date(),
     location: 'ถนนนิวยอร์ก',
