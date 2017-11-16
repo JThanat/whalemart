19141ce9642ad7bc11f04d1c07c1946f7324710e
Kasidit Iamthong <kasidit@outlook.com>
2017-11-16 17:13:23 +0700

    Enable routes preloading

--- frontend-webapp/src/app/app-routing.module.ts
+++ frontend-webapp/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -27,7 +27,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
