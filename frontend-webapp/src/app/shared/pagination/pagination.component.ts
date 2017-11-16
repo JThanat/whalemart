2c8fd80a12fdb7b7b91936a8b48bf54200972e16
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-16 17:34:26 +0700

    Remove OnInit

--- frontend-webapp/src/app/shared/pagination/pagination.component.ts
+++ frontend-webapp/src/app/shared/pagination/pagination.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit, OnChanges {
+export class PaginationComponent implements OnChanges {
   public numCenter: number[];
   public showLeft: boolean;
   public showDotLeft: boolean;
