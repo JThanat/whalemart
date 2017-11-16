c6fd75834146307b339d06abc9162af29b63aa72
Kasidit Iamthong <kasidit@outlook.com>
2017-11-16 21:41:05 +0700

    Simplify SubNavBarService

--- frontend-webapp/src/app/core/nav-bar/sub-nav-bar.service.ts
+++ frontend-webapp/src/app/core/nav-bar/sub-nav-bar.service.ts
@@ -4,26 +4,23 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class SubNavBarService {
-  private isAttaching = false;
   private attachingSubNavBarPortal = new BehaviorSubject<Portal<any> | undefined>(undefined);
 
   attach(subNavBarTemplate: TemplateRef<any>, subNavBarViewContainerRef: ViewContainerRef) {
-    if (this.isAttaching) {
+    if (this.attachingSubNavBarPortal.value) {
       throw new Error('Cannot attach more than one sub nav bar simultaneously');
     }
 
     const portal = new TemplatePortal(subNavBarTemplate, subNavBarViewContainerRef);
     this.attachingSubNavBarPortal.next(portal);
-    this.isAttaching = true;
   }
 
   detach() {
-    if (!this.isAttaching) {
+    if (!this.attachingSubNavBarPortal.value) {
       throw new Error('No sub nav bar to detach');
     }
 
     this.attachingSubNavBarPortal.next(undefined);
-    this.isAttaching = false;
   }
 
   getAttachingSubNavBarPortal() {
