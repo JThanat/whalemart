cee3eb20cdbbdc016b441606ec2a7436ffe8ea6b
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-17 21:39:21 +0700

    Complete pagination logic and test stuffs

--- frontend-webapp/src/app/shared/pagination/pagination.component.spec.ts
+++ frontend-webapp/src/app/shared/pagination/pagination.component.spec.ts
@@ -6,6 +6,30 @@ describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
+  function mergePageNums(comp: PaginationComponent): number[] {
+    const { left, center, right } = comp;
+    let arr: number[] = [];
+    if (left) {
+      arr = arr.concat([left]);
+    }
+    arr = arr.concat(center);
+    if (right) {
+      arr = arr.concat([right]);
+    }
+    return arr;
+  }
+
+  function expectPaginationProps(
+    comp: PaginationComponent,
+    showDotLeft: boolean,
+    centerElm: number[],
+    showDotRight: boolean
+  ) {
+    expect(comp.showDotLeft).toBe(showDotLeft);
+    expect(mergePageNums(comp)).toEqual(centerElm);
+    expect(comp.showDotRight).toBe(showDotRight);
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ PaginationComponent ]
@@ -16,7 +40,233 @@ describe('PaginationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+  });
+
+  describe('pagination with <= 4 elements', () => {
+    it('should display 1 element when given 1 element', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = 1;
+      component.ngOnChanges();
+
+      expectPaginationProps(component, false, [1], false);
+    });
+
+    it('should display 2 elements when given 2 elemetns', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = 2;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2], false);
+    });
+
+    it('should display 3 elements when given 3 elements', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = 3;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3], false);
+    });
+
+    it('should display 4 elements when given 4 elements', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = 4;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4], false);
+    });
+  });
+
+  describe('pagination with 5 elements', () => {
+    it('should display 5 element when current position is 1', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4, 5], false);
+    });
+
+    it('should display 5 element when current position is 2', () => {
+      component.min = 1;
+      component.current = 2;
+      component.max = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4, 5], false);
+    });
+
+    it('should display 5 element when current position is 3', () => {
+      component.min = 1;
+      component.current = 3;
+      component.max = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4, 5], false);
+    });
+
+    it('should display 5 element when current position is 4', () => {
+      component.min = 1;
+      component.current = 4;
+      component.max = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4, 5], false);
+    });
+
+    it('should display 5 element when current position is 5', () => {
+      component.min = 1;
+      component.current = 5;
+      component.max = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 4, 5], false);
+    });
+  });
+
+  describe('pagination with custom min/max', () => {
+    it('should display correctly when max is large', () => {
+      component.min = 1;
+      component.current = 170;
+      component.max = 999;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, true, [1, 169, 170, 171, 999], true);
+    });
+
+    it('should display correctly when min and max is same number', () => {
+      component.min = 16;
+      component.current = 16;
+      component.max = 16;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [16], false);
+    });
+
+    it('should display right dot', () => {
+      component.min = 100;
+      component.current = 101;
+      component.max = 500;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [100, 101, 102, 103, 500], true);
+    });
+
+    it('should display left dot', () => {
+      component.min = 100;
+      component.current = 499;
+      component.max = 500;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, true, [100, 497, 498, 499, 500], false);
+    });
+  });
+
+  describe('pagination with custom padding', () => {
+    it('should display 7 elements when padding is 2', () => {
+      component.min = 5;
+      component.current = 16;
+      component.max = 30;
+      component.padding = 2;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, true, [5, 14, 15, 16, 17, 18, 30], true);
+    });
+
+    it('should display 7 elements when padding is 2 and current is on left', () => {
+      component.min = 5;
+      component.current = 5;
+      component.max = 20;
+      component.padding = 2;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [5, 6, 7, 8, 9, 20], true);
+    });
+
+    it('should display 7 elements when padding is 2 and current is on right', () => {
+      component.min = 5;
+      component.current = 20;
+      component.max = 20;
+      component.padding = 2;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, true, [5, 16, 17, 18, 19, 20], false);
+    });
+
+    it('should display 13 elements when padding is 5', () => {
+      component.min = 10;
+      component.current = 50;
+      component.max = 100;
+      component.padding = 5;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(
+        component,
+        true,
+        [10, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 100],
+        true
+      );
+    });
+  });
+
+  describe('pagination with edge case', () => {
+    it('should display only single element when max is lesser than min', () => {
+      component.min = 1;
+      component.current = 1;
+      component.max = -123;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1], false);
+    });
+
+    it('should display correct position when given position is incorrect', () => {
+      component.min = 1;
+      component.current = -123;
+      component.max = 10;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, false, [1, 2, 3, 10], true);
+      expect(component.current).toBe(1);
+
+      component.current = 123;
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expectPaginationProps(component, true, [1, 8, 9, 10], false);
+      expect(component.current).toBe(10);
+    });
+  });
+
+  it('should change the dot to number when edge number is adjacent with center', () => {
+    component.min = 4;
+    component.current = 8;
+    component.max = 12;
+    component.padding = 2;
+    component.ngOnChanges();
     fixture.detectChanges();
+
+    expectPaginationProps(component, false, [4, 5, 6, 7, 8, 9, 10, 11, 12], false);
   });
 
   it('should create', () => {
--- frontend-webapp/src/app/shared/pagination/pagination.component.ts
+++ frontend-webapp/src/app/shared/pagination/pagination.component.ts
@@ -6,10 +6,10 @@ import { Component, Input, OnChanges } from '@angular/core';
   styleUrls: ['./pagination.component.scss']
 })
 export class PaginationComponent implements OnChanges {
-  public numCenter: number[];
-  public showLeft: boolean;
+  public left: number;
+  public right: number;
+  public center: number[];
   public showDotLeft: boolean;
-  public showRight: boolean;
   public showDotRight: boolean;
 
   @Input() min = 1;
@@ -18,30 +18,80 @@ export class PaginationComponent implements OnChanges {
   @Input() padding = 1;
 
   ngOnChanges() {
+    if (this.max < this.min) {
+      this.max = this.min;
+    }
+
     if (this.current < this.min) {
       this.current = this.min;
     } else if (this.current > this.max) {
       this.current = this.max;
     }
 
-    this.showLeft = (this.current - this.min > this.padding);
-    this.showRight = (this.max - this.current > this.padding);
+    const pageNums: number[] = [];
 
-    const centerLeft = Math.max(
-      this.current - this.padding,
-      this.min
-    );
-    const centerRight = Math.min(
-      this.current + this.padding,
-      this.max
-    );
-
-    this.showDotLeft = this.min + 1 < centerLeft;
-    this.showDotRight = this.max - 1 > centerRight;
-
-    this.numCenter = [];
-    for (let i = centerLeft; i <= centerRight; i++) {
-      this.numCenter.push(i);
+    const center = this.current;
+    if (center > this.min && center < this.max) {
+      pageNums.push(center);
     }
+
+    for (let i = 1, j = 1; i <= this.padding; i++) {
+      const leftPad = center - j;
+      const rightPad = center + j;
+
+      if (leftPad > this.min && rightPad < this.max) {
+        pageNums.unshift(leftPad);
+        pageNums.push(rightPad);
+        j += 1;
+      } else if (leftPad > this.min) {
+        pageNums.unshift(leftPad);
+        if (leftPad - 1 > this.min) {
+          pageNums.unshift(leftPad - 1);
+        }
+        j += 2;
+      } else if (rightPad < this.max) {
+        pageNums.push(rightPad);
+        if (rightPad + 1 < this.max) {
+          pageNums.push(rightPad + 1);
+        }
+        j += 2;
+      }
+    }
+
+    this.showDotLeft = false;
+    this.showDotRight = false;
+
+    if (pageNums.length > 0) {
+      if (this.min + 2 === pageNums[0]) {
+        pageNums.unshift(this.min + 1);
+      }
+      if (this.max - 2 === pageNums[pageNums.length - 1]) {
+        pageNums.push(this.max - 1);
+      }
+    }
+
+    pageNums.unshift(this.min);
+    if (this.min !== this.max) {
+      pageNums.push(this.max);
+    }
+
+    if (pageNums.length > 1) {
+      if (pageNums[0] + 1 !== pageNums[1]) {
+        this.showDotLeft = true;
+      }
+      if (pageNums[pageNums.length - 2] + 1 !== pageNums[pageNums.length - 1]) {
+        this.showDotRight = true;
+      }
+    }
+
+    const left = pageNums.shift();
+    if (left) { this.left = left; }
+
+    if (pageNums.length > 0) {
+      const right = pageNums.pop();
+      if (right) { this.right = right; }
+    }
+
+    this.center = pageNums;
   }
 }
