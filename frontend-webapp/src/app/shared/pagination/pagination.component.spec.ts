09274dceb603ac87129180711ea1982ed1b177be
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-18 00:26:08 +0700

    Change fdescribe to describe

--- frontend-webapp/src/app/shared/pagination/pagination.component.spec.ts
+++ frontend-webapp/src/app/shared/pagination/pagination.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { PaginationComponent } from './pagination.component';
 
-fdescribe('PaginationComponent', () => {
+describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
 
