4cbdb7e6eece1a216f2b457555411344842ed553
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-19 01:26:20 +0700

    Add serializer fields

--- webserver/apps/rental_payment_info/serializers.py
+++ webserver/apps/rental_payment_info/serializers.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+
 from rest_framework import serializers
 
+from apps.rental_payment_info.models import RentalPaymentInfo, Installment
 
-class RentalPaymentSerializer(serializers.Serializer):
+
+class InstallmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Installment
+        fields = ('id', 'payment_method', 'round', 'payment_date', 'amount', 'credit_card', 'receipt_image',
+                  'is_verified', 'rental_payment_info')
+
     def create(self, validated_data):
         pass
 
