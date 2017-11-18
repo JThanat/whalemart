9f51c42e52e9651f2dc7cfb7fa7142154f4154f0
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-19 01:55:34 +0700

    Add payment type

--- webserver/apps/rental_payment_info/serializers.py
+++ webserver/apps/rental_payment_info/serializers.py
@@ -6,10 +6,18 @@ from apps.rental_payment_info.models import RentalPaymentInfo, Installment
 
 
 class InstallmentSerializer(serializers.ModelSerializer):
+    PARTIAL = 1
+    FULL = 2
+    PAYMENT_TYPE_CHOICES = (
+        (PARTIAL, 'Partial'),
+        (FULL, 'Full')
+    )
+    payment_type = serializers.ChoiceField(choices=PAYMENT_TYPE_CHOICES, label='Payment Type', write_only=True)
+
     class Meta:
         model = Installment
-        fields = ('id', 'payment_method', 'round', 'payment_date', 'amount', 'credit_card', 'receipt_image',
-                  'is_verified', 'rental_payment_info')
+        fields = ('id', 'payment_type', 'payment_method', 'round', 'payment_date', 'amount', 'credit_card',
+                  'receipt_image', 'is_verified', 'rental_payment_info')
 
     def create(self, validated_data):
         pass
