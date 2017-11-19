3e9218da65712a631b7260aa7254ef4e0c78e60e
Korrawe <korrawe@gmail.com>
2017-11-20 02:57:48 +0700

    Move send_notification() into Class Notifier

--- webserver/apps/commons/send_notification.py
+++ webserver/apps/commons/send_notification.py
@@ -1,15 +1,19 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.db import models
 
 
-def send_notification(subject, content, recipient_list):
+class Notifier(models.Model):
 	"""
-	Send notification email to recipients in recipient_list
-	:subject: email subject
-	:content: email content
-	:recipient_list: a list of string, emails of the recipients
-	:return: a number of successfully delivered messages 
+	Provide a model for sending email to recipients
 	"""
-
-	return send_mail(subject, content, settings.EMAIL_HOST_USER, 
-		recipient_list, fail_silently=False)
+	def send_notification(subject, content, recipient_list):
+		"""
+		Send notification email to recipients in recipient_list
+		:subject: email subject
+		:content: email content
+		:recipient_list: a list of string, emails of the recipients
+		:return: a number of successfully delivered messages 
+		"""
+		return send_mail(subject, content, settings.EMAIL_HOST_USER, 
+			recipient_list, fail_silently=False)
