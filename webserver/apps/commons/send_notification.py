44c629b981cbd62fc1b27ce2b5bb7284262110a4
Korrawe <korrawe@gmail.com>
2017-11-20 04:12:07 +0700

    Revert "Move send_notification() into Class Notifier"
    This reverts commit 3e9218da65712a631b7260aa7254ef4e0c78e60e.

--- webserver/apps/commons/send_notification.py
+++ webserver/apps/commons/send_notification.py
@@ -1,19 +1,15 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.db import models
 
 
-class Notifier(models.Model):
+def send_notification(subject, content, recipient_list):
 	"""
-	Provide a model for sending email to recipients
+	Send notification email to recipients in recipient_list
+	:subject: email subject
+	:content: email content
+	:recipient_list: a list of string, emails of the recipients
+	:return: a number of successfully delivered messages 
 	"""
-	def send_notification(subject, content, recipient_list):
-		"""
-		Send notification email to recipients in recipient_list
-		:subject: email subject
-		:content: email content
-		:recipient_list: a list of string, emails of the recipients
-		:return: a number of successfully delivered messages 
-		"""
-		return send_mail(subject, content, settings.EMAIL_HOST_USER, 
-			recipient_list, fail_silently=False)
+
+	return send_mail(subject, content, settings.EMAIL_HOST_USER, 
+		recipient_list, fail_silently=False)
