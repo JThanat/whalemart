54a1f84e85448d7d3fc900ed1bc66abebf1e7d94
Kasidit Iamthong <kasidit@outlook.com>
2017-11-20 10:48:38 +0700

    Fix unit tests

--- frontend-webapp/src/app/shared/date-range/date-range-input.directive.spec.ts
+++ frontend-webapp/src/app/shared/date-range/date-range-input.directive.spec.ts
@@ -2,7 +2,7 @@ import { DateRangeInputDirective } from './date-range-input.directive';
 
 describe('DateRangeInputDirective', () => {
   it('should create an instance', () => {
-    const directive = new DateRangeInputDirective();
+    const directive = new DateRangeInputDirective(null as any, null as any);
     expect(directive).toBeTruthy();
   });
 });
