49140ef9c75b8393a87306af0685a02f7b0e0db6
Kasidit Iamthong <kasidit@outlook.com>
2017-11-23 05:35:38 +0700

    Fix unit tests

--- frontend-webapp/src/app/shared/menu/menu.component.spec.ts
+++ frontend-webapp/src/app/shared/menu/menu.component.spec.ts
@@ -1,3 +1,5 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuComponent } from './menu.component';
@@ -6,12 +8,15 @@ describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [OverlayModule],
+        declarations: [MenuComponent],
+        schemas: [NO_ERRORS_SCHEMA]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MenuComponent);
