68e7b27f7191af018ab16e5428db12e72e966525
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-23 06:11:46 +0700

    Apply canActivate graud to lessor-dashboard route that check that user are lessor or not

--- frontend-webapp/src/app/lessor-dashboard/lessor-dashboard-routing.module.ts
+++ frontend-webapp/src/app/lessor-dashboard/lessor-dashboard-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { IsLessorGuardService } from './is-lessor-guard.service';
 import {LessorDashboardComponent} from './lessor-dashboard.component';
 const routes: Routes = [
   {
     path: '',
-    component: LessorDashboardComponent
+    component: LessorDashboardComponent,
+    canActivate: [IsLessorGuardService]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [IsLessorGuardService]
 })
 export class LessorDashboardRoutingModule { }
