e1963d4ac190fe4c31ca41c3e0eca84a224a895c
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-22 15:40:59 +0700

    Finish serializer

--- webserver/apps/reservations/serializers.py
+++ webserver/apps/reservations/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
-from .models import Reservation
+from .models import Reservation, ReservedBooth
+
+
+class ReservedBoothSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservedBooth
+        fields = ('id', 'status', 'booth')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'status': {'read_only': True},
+        }
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    reserved_booths = ReservedBoothSerializer(many=True)
 
     class Meta:
         model = Reservation
-        fields = ('id', 'shop_name', 'reservation_time', 'status', 'booth')
+        fields = ('id', 'shop_name', 'reservation_time', 'status', 'market')
         extra_kwargs = {
-            'status': { 'read_only': True },
-            'reservation_time': { 'read_only': True },
+            'id': {'read_only': True},
+            'status': {'read_only': True},
+            'reservation_time': {'read_only': True},
         }
-    #
-    # de f create(self, validated_data):
-    #     validated_data['user'] = self.context['request'].user
-    # return MarketReservation.objects.create(**validated_data)
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        reserved_booths = validated_data.pop('booths', None)
+        reservation = Reservation.objects.create(**validated_data)
+        for booth in reserved_booths:
+            ReservedBooth.objects.create(reservation=reservation, booth=booth, status=ReservedBooth.PENDING)
+        return reservation
