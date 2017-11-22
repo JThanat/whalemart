14388a4bb7746ecab68bf24d74da0624f9940bbb
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-23 00:37:03 +0700

    Create second installment

--- webserver/apps/payments/models.py
+++ webserver/apps/payments/models.py
@@ -12,11 +12,11 @@ class RentalPaymentInfo(models.Model):
     )
     status = models.IntegerField(verbose_name='Status', choices=STATUS_CHOICES, default=DRAFTED)
     user = models.ForeignKey('users.User', verbose_name='User', related_name='rental_payment_info')
-    reservation_info = models.ForeignKey('market_reservations.MarketReservation', verbose_name='Market Reservation',
+    reservation = models.ForeignKey('reservations.Reservation', verbose_name='Reservation',
                                          related_name='rental_payment_infos')
 
     def __str__(self):
-        return '%s %s' % (self.user.first_name, self.reservation_info)
+        return '%s %s' % (self.user.first_name, self.reservation)
 
 
 class Installment(models.Model):
@@ -30,7 +30,6 @@ class Installment(models.Model):
     round = models.IntegerField(verbose_name='Round')
     payment_date = models.DateField(verbose_name='Payment Date', auto_now_add=True)
     amount = models.DecimalField(verbose_name='Amount', max_digits=15, decimal_places=2)
-    credit_card = models.ForeignKey('users.CreditCard', null=True, verbose_name='Credit Card')
     receipt_image = models.ImageField(verbose_name='Receipt Image', null=True, upload_to='receipt_images/%Y/%m/%d')
     PENDING = 1
     APPROVED = 2
--- webserver/apps/payments/serializers.py
+++ webserver/apps/payments/serializers.py
@@ -4,29 +4,18 @@ from rest_framework import serializers
 
 from apps.reservations.models import Reservation
 from apps.payments.models import RentalPaymentInfo, Installment
+from apps.markets.models import Market
 from apps.users.models import CreditCard
 from apps.users.serializers import CreditCardSerializer
 
 
-class FirstInstallmentSerializer(serializers.ModelSerializer):
-    PARTIAL = 1
-    FULL = 2
-    PAYMENT_TYPE_CHOICES = (
-        (PARTIAL, 'Partial'),
-        (FULL, 'Full')
-    )
-    payment_type = serializers.ChoiceField(choices=PAYMENT_TYPE_CHOICES, label='Payment Type', write_only=True)
-    reservation_info = serializers.PrimaryKeyRelatedField(many=False, queryset=Reservation.objects.all(),
-                                                          write_only=True, label='Reservation Info')
+class InstallmentSerializer(serializers.ModelSerializer):
+    market = serializers.PrimaryKeyRelatedField(many=False, queryset=Market.objects.all(), write_only=True,
+                                                label='Market')
+    credit_card = serializers.PrimaryKeyRelatedField(many=False, queryset=CreditCard.objects.all(), write_only=True,
+                                                     label='Credit Card')
     new_credit_card = CreditCardSerializer(many=False, required=False)
-
-    class Meta:
-        model = Installment
-        fields = ('id', 'payment_type', 'reservation_info', 'new_credit_card', 'payment_method', 'payment_date',
-                  'amount', 'credit_card', 'receipt_image', 'verification_status')
-        extra_kwargs = {
-            'verification_status': {'read_only': True}
-        }
+    save_new_credit_card = serializers.BooleanField(default=False, label='Save new credit card')
 
     def validate_new_credit_card(self, card_info):
         success, message = pay_with_credit_card(**card_info)
@@ -41,11 +30,30 @@ class FirstInstallmentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(message)
         return card_info
 
+
+class FirstInstallmentSerializer(InstallmentSerializer):
+    PARTIAL = 1
+    FULL = 2
+    PAYMENT_TYPE_CHOICES = (
+        (PARTIAL, 'Partial'),
+        (FULL, 'Full')
+    )
+    payment_type = serializers.ChoiceField(choices=PAYMENT_TYPE_CHOICES, label='Payment Type', write_only=True)
+
+    class Meta:
+        model = Installment
+        fields = ('id', 'payment_type', 'market', 'credit_card', 'new_credit_card', 'save_new_credit_card',
+                  'payment_method', 'payment_date', 'amount', 'receipt_image', 'verification_status')
+        extra_kwargs = {
+            'verification_status': {'read_only': True}
+        }
+
     def create(self, validated_data):
         # Create new Rental Payment Info
         payment_type = validated_data.pop('payment_type')
-        reservation_info = validated_data.pop('reservation_info')
+        market = validated_data.pop('market')
         user = self.context['request'].user
+        reservation = Reservation.objects.filter(user=user, market=market)
         status = RentalPaymentInfo.DRAFTED
         if validated_data['payment_method'] == Installment.CREDIT_CARD:
             validated_data['verification_status'] = Installment.APPROVED
@@ -54,13 +62,47 @@ class FirstInstallmentSerializer(serializers.ModelSerializer):
             elif payment_type == self.FULL:
                 status = RentalPaymentInfo.FULLY_PAID
         rental_payment_info = RentalPaymentInfo.objects.create(user=user, status=status,
-                                                               reservation_info=reservation_info)
+                                                               reservation=reservation)
         # Create new credit card if user pay with new credit card
-        new_credit_card = validated_data.pop('new_credit_card')
+        new_credit_card = validated_data.pop('new_credit_card', None)
+        save_new_credit_card = validated_data.pop('save_new_credit_card', False)
+        _ = validated_data.pop('credit_card', None)
         new_credit_card['user'] = user
-        validated_data['credit_card'] = CreditCard.object.create(**new_credit_card)
+        if save_new_credit_card:
+            CreditCard.object.create(**new_credit_card)
         # Create First Installment
         validated_data['round'] = 1
+        validated_data['rental_payment_info'] = rental_payment_info
+        return Installment.objects.create(**validated_data)
+
+
+class SecondInstallmentSerializer(InstallmentSerializer):
+    class Meta:
+        model = Installment
+        fields = ('id', 'market', 'credit_card', 'new_credit_card', 'save_new_credit_card',
+                  'payment_method', 'payment_date', 'amount', 'receipt_image', 'verification_status')
+        extra_kwargs = {
+            'verification_status': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        # Rental Payment Info
+        market = validated_data.pop('market')
+        user = self.context['request'].user
+        reservation = Reservation.objects.get(user=user, market=market)
+        rental_payment_info = RentalPaymentInfo.objects.get(user=user, reservation=reservation)
+        if validated_data['payment_method'] == Installment.CREDIT_CARD:
+            validated_data['verification_status'] = Installment.APPROVED
+            validated_data['status'] = RentalPaymentInfo.FULLY_PAID
+        # Create new credit card if user pay with new credit card
+        new_credit_card = validated_data.pop('new_credit_card', None)
+        save_new_credit_card = validated_data.pop('save_new_credit_card', False)
+        _ = validated_data.pop('credit_card', None)
+        new_credit_card['user'] = user
+        if save_new_credit_card:
+            CreditCard.object.create(**new_credit_card)
+        # Create First Installment
+        validated_data['round'] = 2
         validated_data['rental_payment_info'] = rental_payment_info
         return Installment.objects.create(**validated_data)
 
