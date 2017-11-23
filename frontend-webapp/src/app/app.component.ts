4c6719862aab47a62949f00546ba8a8239eea6e9
Kasidit Iamthong <kasidit@outlook.com>
2017-11-23 07:57:19 +0700

    Do not scroll to top if only fragment changes

--- frontend-webapp/src/app/app.component.ts
+++ frontend-webapp/src/app/app.component.ts
@@ -8,12 +8,23 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  private previousUrl = '';
   constructor(private router: Router) {}
 
   ngOnInit() {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        window.scroll(0, 0);
+        const newUrlTree = this.router.parseUrl(event.url);
+        newUrlTree.fragment = '';
+
+        const previousUrl = this.router.parseUrl(this.previousUrl);
+        previousUrl.fragment = '';
+
+        if (this.router.serializeUrl(newUrlTree) !== this.router.serializeUrl(previousUrl)) {
+          window.scroll(0, 0);
+        }
+
+        this.previousUrl = event.url;
       }
     });
   }
