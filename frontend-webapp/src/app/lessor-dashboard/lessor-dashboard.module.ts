3758c5892b587ca5802abd5d354fd0be8e7ac54f
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-24 22:33:42 +0700

    Add new component to module

--- frontend-webapp/src/app/lessor-dashboard/lessor-dashboard.module.ts
+++ frontend-webapp/src/app/lessor-dashboard/lessor-dashboard.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { LessorDashboardRoutingModule } from './lessor-dashboard-routing.module';
 import { LessorDashboardComponent } from './lessor-dashboard.component';
+import { LessorInfoComponent } from './lessor-info/lessor-info.component';
+import { LessorManageMarketComponent } from './lessor-manage-market/lessor-manage-market.component';
 
 @NgModule({
   imports: [
     SharedModule,
     LessorDashboardRoutingModule
   ],
-  declarations: [LessorDashboardComponent]
+  declarations: [LessorDashboardComponent, LessorInfoComponent, LessorManageMarketComponent]
 })
 export class LessorDashboardModule { }
