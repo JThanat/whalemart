49384a42f581ab0cb33ec4eaf28234433fcb66cb
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-24 22:28:38 +0700

    Create lessor info component

---
+++ frontend-webapp/src/app/lessor-dashboard/lessor-info/lessor-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LessorInfoComponent } from './lessor-info.component';
+
+describe('LessorInfoComponent', () => {
+  let component: LessorInfoComponent;
+  let fixture: ComponentFixture<LessorInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LessorInfoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LessorInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend-webapp/src/app/lessor-dashboard/lessor-info/lessor-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lessor-info',
+  templateUrl: './lessor-info.component.html',
+  styleUrls: ['./lessor-info.component.scss']
+})
+export class LessorInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
