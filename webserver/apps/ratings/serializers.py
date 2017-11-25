7760bc6b180f60c154c47c14a4ceb6d2655c78ef
Korrawe <korrawe@gmail.com>
2017-11-25 16:03:20 +0700

    Change score range from [0,5] to [1,5]

--- webserver/apps/ratings/serializers.py
+++ webserver/apps/ratings/serializers.py
@@ -9,6 +9,6 @@ class RatingSerializer(serializers.ModelSerializer):
         fields = ('rating_score', 'time_stamp', 'user', 'market')
 
     def validate_rating_score(self, data):
-        if data < 0 or data > 5:
+        if data < 1 or data > 5:
             raise serializers.ValidationError('Rating should be an integer between 1 to 5')
         return data
