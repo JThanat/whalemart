38e4e4931ba958d0e7a2528bee9e754881690dfa
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-11-25 17:04:08 +0700

    Change serializer of payment viewset

--- webserver/apps/payments/views.py
+++ webserver/apps/payments/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
-from apps.payments.models import Installment
-from apps.payments.serializers import FirstInstallmentSerializer
+from apps.payments.models import Installment, RentalPaymentInfo
+from apps.payments.serializers import InstallmentSerializer
 
 
-class FirstInstallmentViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+class PaymentViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
     queryset = Installment.objects.all()
-    serializer_class = FirstInstallmentSerializer()
+    serializer_class = InstallmentSerializer
+    permission_classes = (IsAuthenticated,)
