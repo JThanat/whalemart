4d630b40fc78ad771054ea9e4f31fbc688335ab7
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-26 05:58:27 +0700

    Fix test

--- frontend-webapp/src/app/vendor-dashboard/product/vendor-product.service.spec.ts
+++ frontend-webapp/src/app/vendor-dashboard/product/vendor-product.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
 
 import { VendorProductService } from './vendor-product.service';
@@ -5,11 +6,15 @@ import { VendorProductService } from './vendor-product.service';
 describe('VendorProductService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [VendorProductService]
     });
   });
 
-  it('should be created', inject([VendorProductService], (service: VendorProductService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([VendorProductService], (service: VendorProductService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
