6ad4c09c893de42100b8a588c185c58571bbdaf1
Kasidit Iamthong <kasidit@outlook.com>
2017-11-26 02:49:34 +0700

    Use marketRouterLink for overriding href instead

--- frontend-webapp/src/app/app.module.ts
+++ frontend-webapp/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
-
 @NgModule({
   declarations: [
     AppComponent
