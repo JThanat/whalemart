06aa8985815752eeb9b4dca894229fc9c02484a8
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-27 03:30:13 +0700

    Write api to get report list

--- frontend-webapp/src/app/admin-dashboard/report/report.service.ts
+++ frontend-webapp/src/app/admin-dashboard/report/report.service.ts
@@ -1,8 +1,44 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http/src/client';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { _throw as observableThrow } from 'rxjs/observable/throw';
+import { catchError, map } from 'rxjs/operators';
 
+
+export interface Report {
+  report_content: string;
+  time_stamp: Date;
+  user: number;
+  market: number;
+  username: string;
+}
+
+interface ReportServerResponse {
+  count: number;
+  next: string;
+  previous: string;
+  results: Report[];
+}
+
+export class ReportError {}
 @Injectable()
 export class ReportService {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  getReportList(): Observable<Report[]> {
+    return this.http.get<ReportServerResponse>('/api/report/').pipe(
+      map(data => {
+        return data.results;
+      }),
+      catchError((err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status >= 400 && err.status < 500) {
+            return observableThrow(new ReportError()) as Observable<Report[]>;
+          }
+        }
+        return observableThrow(err) as Observable<Report[]>;
+      })
+    );
+  }
 }
