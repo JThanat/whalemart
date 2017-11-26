1cd10b76f89a6aa488f6eef8d4ca03d1e259004c
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-26 23:17:07 +0700

    Create receipt service

--- frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.component.ts
+++ frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-class Receipt {
-  id: string;
-  date: Date;
-  profileImage: string;
-  amount: number;
-}
-
+import { Receipt, ReceiptService } from './receipt.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-admin-verify-receipt',
@@ -15,35 +9,44 @@ class Receipt {
 })
 export class AdminVerifyReceiptComponent implements OnInit {
   receipt1: Receipt = {
-    id: '1234',
-    date: new Date(),
-    profileImage:
+    id: 1234,
+    payment_date: '2017-02-02',
+    receipt_image:
       'https://acme.invoicehome.com/assets/invoice_templates/en/receipt/88-74f61a66097977e2774417863164fe2dafe5e22e94787c4bc01063ece78f7bc4.png',
-    amount: 1234
+    amount: 1234,
+    verification_status: 1
   };
-
   receipt2: Receipt = {
-    id: '1234',
-    date: new Date(),
-    profileImage: 'https://discourse-cdn-sjc1.com/business2/uploads/manager1/original/2X/7/71bfea85ebeba2ed4409a76cb38b8f3336847440.png',
-    amount: 1234
+    id: 1234,
+    payment_date: '2017-02-02',
+    receipt_image:
+      'https://acme.invoicehome.com/assets/invoice_templates/en/receipt/88-74f61a66097977e2774417863164fe2dafe5e22e94787c4bc01063ece78f7bc4.png',
+    amount: 1234,
+    verification_status: 1
   };
+ 
 
-  receipts: Receipt[] = [
-    this.receipt1,
-    this.receipt2,
-    {
-      id: '1234',
-      date: new Date(),
-      profileImage:
-      'http://www.makereceipts.com/receipt_preview.jpg',
-      amount: 1234
-    };
-  ];
+  receipts$: Observable < Receipt[]>;
+  // = [
+  //   this.receipt1,
+  //   this.receipt2,
+  //   {
+  //     id: '1234',
+  //     date: new Date(),
+  //     profileImage:
+  //     'http://www.makereceipts.com/receipt_preview.jpg',
+  //     amount: 1234
+  //   }
+  // ];
 
-  constructor() { }
+  constructor(private receiptService: ReceiptService) { }
+
 
   ngOnInit() {
+    this.receipts$ = this.receiptService.getReceiptList()
+    // .subscribe(data => {
+    //   this.receipts$ = data;
+    // });
   }
 
 }
--- frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.module.ts
+++ frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AdminVerifyReceiptRoutingModule } from './admin-verify-receipt-routing.module';
 import { AdminVerifyReceiptComponent } from './admin-verify-receipt.component';
+import { ReceiptService } from './receipt.service';
 
 
 @NgModule({
@@ -9,6 +10,7 @@ import { AdminVerifyReceiptComponent } from './admin-verify-receipt.component';
     SharedModule,
     AdminVerifyReceiptRoutingModule
   ],
-  declarations: [AdminVerifyReceiptComponent]
+  declarations: [AdminVerifyReceiptComponent],
+  providers: [ReceiptService]
 })
 export class AdminVerifyReceiptModule { }
