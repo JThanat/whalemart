bca618d613b761eb5c123b38e77f6901bfda2c5b
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-27 03:28:42 +0700

    Add market name to report

--- webserver/apps/reports/serializers.py
+++ webserver/apps/reports/serializers.py
@@ -5,11 +5,10 @@ from .models import Report
 import re
 
 class ReportSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    market = serializers.StringRelatedField()
+
     class Meta:
         model = Report
         fields = ('report_content', 'time_stamp', 'user', 'market')
 
-    def to_representation(self, instance):
-        repr = super(ReportSerializer, self).to_representation(instance)
-        repr['username'] = str(User.objects.get(pk=repr['user']))
-        return repr
