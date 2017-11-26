423de77939fef31f5cbeaf810ef31ab0fcdcd42b
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-26 23:17:36 +0700

    Clearn up code

--- frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.component.ts
+++ frontend-webapp/src/app/admin-verify-receipt/admin-verify-receipt.component.ts
@@ -8,45 +8,15 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./admin-verify-receipt.component.scss']
 })
 export class AdminVerifyReceiptComponent implements OnInit {
-  receipt1: Receipt = {
-    id: 1234,
-    payment_date: '2017-02-02',
-    receipt_image:
-      'https://acme.invoicehome.com/assets/invoice_templates/en/receipt/88-74f61a66097977e2774417863164fe2dafe5e22e94787c4bc01063ece78f7bc4.png',
-    amount: 1234,
-    verification_status: 1
-  };
-  receipt2: Receipt = {
-    id: 1234,
-    payment_date: '2017-02-02',
-    receipt_image:
-      'https://acme.invoicehome.com/assets/invoice_templates/en/receipt/88-74f61a66097977e2774417863164fe2dafe5e22e94787c4bc01063ece78f7bc4.png',
-    amount: 1234,
-    verification_status: 1
-  };
- 
 
   receipts$: Observable < Receipt[]>;
-  // = [
-  //   this.receipt1,
-  //   this.receipt2,
-  //   {
-  //     id: '1234',
-  //     date: new Date(),
-  //     profileImage:
-  //     'http://www.makereceipts.com/receipt_preview.jpg',
-  //     amount: 1234
-  //   }
-  // ];
 
   constructor(private receiptService: ReceiptService) { }
 
 
   ngOnInit() {
-    this.receipts$ = this.receiptService.getReceiptList()
-    // .subscribe(data => {
-    //   this.receipts$ = data;
-    // });
+    this.receipts$ = this.receiptService.getReceiptList();
+
   }
 
 }
