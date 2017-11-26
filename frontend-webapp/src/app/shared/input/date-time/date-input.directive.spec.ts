5be2c91155c205b61364d65f2968cdd83f03c9cb
Kasidit Iamthong <kasidit@outlook.com>
2017-11-27 05:01:41 +0700

    Fix compile error

--- frontend-webapp/src/app/shared/input/date-time/date-input.directive.spec.ts
+++ frontend-webapp/src/app/shared/input/date-time/date-input.directive.spec.ts
@@ -2,7 +2,7 @@ import { DateInputDirective } from './date-input.directive';
 
 describe('DateInputDirective', () => {
   it('should create an instance', () => {
-    const directive = new DateInputDirective();
+    const directive = new DateInputDirective(null as any);
     expect(directive).toBeTruthy();
   });
 });
--- frontend-webapp/src/app/shared/input/date-time/time-input.directive.spec.ts
+++ frontend-webapp/src/app/shared/input/date-time/time-input.directive.spec.ts
@@ -2,7 +2,7 @@ import { TimeInputDirective } from './time-input.directive';
 
 describe('TimeInputDirective', () => {
   it('should create an instance', () => {
-    const directive = new TimeInputDirective();
+    const directive = new TimeInputDirective(null as any);
     expect(directive).toBeTruthy();
   });
 });
