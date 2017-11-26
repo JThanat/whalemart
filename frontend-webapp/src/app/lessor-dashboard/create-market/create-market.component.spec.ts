6dce4eaf1a5e007d65f4bb0b7cb404a31e618189
Kasidit Iamthong <kasidit@outlook.com>
2017-11-27 04:03:42 +0700

    Fix unit tests

--- frontend-webapp/src/app/lessor-dashboard/create-market/create-market.component.spec.ts
+++ frontend-webapp/src/app/lessor-dashboard/create-market/create-market.component.spec.ts
@@ -1,17 +1,43 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of as observableOf } from 'rxjs/observable/of';
 
+import { AlertService } from '../../core/alert/alert.service';
+import { FileInputComponent } from '../../shared/input/file/file-input.component';
 import { CreateMarketComponent } from './create-market.component';
+import { CreateMarketService } from './create-market.service';
+
+class MockCreateMarketService {
+  createMarket() {
+    return observableOf(31);
+  }
+
+  tagsValidators() {
+    return null;
+  }
+}
+
+class MockAlertService {}
 
 describe('CreateMarketComponent', () => {
   let component: CreateMarketComponent;
   let fixture: ComponentFixture<CreateMarketComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateMarketComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [ReactiveFormsModule, RouterTestingModule],
+        declarations: [CreateMarketComponent, FileInputComponent],
+        providers: [
+          { provide: CreateMarketService, useClass: MockCreateMarketService },
+          { provide: AlertService, useClass: MockAlertService }
+        ],
+        schemas: [NO_ERRORS_SCHEMA]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateMarketComponent);
--- frontend-webapp/src/app/lessor-dashboard/create-market/create-market.service.spec.ts
+++ frontend-webapp/src/app/lessor-dashboard/create-market/create-market.service.spec.ts
@@ -1,15 +1,21 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
 
+import { TimeService } from '../../core/utils/time.service';
 import { CreateMarketService } from './create-market.service';
 
 describe('CreateMarketService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CreateMarketService]
+      imports: [HttpClientTestingModule],
+      providers: [CreateMarketService, TimeService]
     });
   });
 
-  it('should be created', inject([CreateMarketService], (service: CreateMarketService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([CreateMarketService], (service: CreateMarketService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
