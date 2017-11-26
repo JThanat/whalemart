995d766bfd607d7bfefd2154f9690715b3174c74
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-27 04:33:53 +0700

    Write test

--- frontend-webapp/src/app/admin-dashboard/report/report.service.spec.ts
+++ frontend-webapp/src/app/admin-dashboard/report/report.service.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReportService } from './report.service';
 
 describe('ReportService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ReportService]
     });
   });
