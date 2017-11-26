af537677f97f336e768aad3afb8d38614d60a7c1
Kawin Liaowongphuthorn <liaokangtai@gmail.com>
2017-11-27 03:13:56 +0700

    Retrieve more infomation to report

--- webserver/apps/reports/serializers.py
+++ webserver/apps/reports/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from apps.users.models import User
 
 from .models import Report
 import re
@@ -7,3 +8,8 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = ('report_content', 'time_stamp', 'user', 'market')
+
+    def to_representation(self, instance):
+        repr = super(ReportSerializer, self).to_representation(instance)
+        repr['username'] = str(User.objects.get(pk=repr['user']))
+        return repr
