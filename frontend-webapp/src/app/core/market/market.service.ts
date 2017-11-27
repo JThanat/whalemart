bb201ad6d99732ab0b406539618c52d2af05d1c1
Kasidit Iamthong <futurizing@users.noreply.github.com>
2017-11-28 03:58:54 +0700

    Remove unused statement

--- frontend-webapp/src/app/core/market/market.service.ts
+++ frontend-webapp/src/app/core/market/market.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-// import { of as observableOf } from 'rxjs/observable/of';
 import { map } from 'rxjs/operators';
 
 import { DateRange } from '../utils/date-range.service';
