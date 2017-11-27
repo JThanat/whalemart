96669bc042a013c67047cc2642dc7cfc99ad436b
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-28 06:06:53 +0700

    Add some upload component

--- frontend-webapp/src/app/payment/upload-receipt/upload-receipt.component.ts
+++ frontend-webapp/src/app/payment/upload-receipt/upload-receipt.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -7,11 +8,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./upload-receipt.component.scss']
 })
 export class UploadReceiptComponent implements OnInit {
+  receiptForm: FormGroup;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       console.log(params);
     });
+
+    this.receiptForm = new FormGroup({
+      image: new FormControl()
+    });
+  }
+
+  uploadReceipt() {
+    if (!this.receiptForm.valid) {
+      return;
+    }
+
+    // const { image } = this.receiptForm.value;
+    // const formData = FormData();
+
   }
 }
