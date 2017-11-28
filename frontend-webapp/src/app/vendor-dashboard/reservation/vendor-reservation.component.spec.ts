fa9eb2a5a98b718c6c80e2c1be6c9bed0fc10717
Kosate Limpongsa <kosatelim@gmail.com>
2017-11-28 15:22:32 +0700

    Fix more test

--- frontend-webapp/src/app/vendor-dashboard/reservation/vendor-reservation.component.spec.ts
+++ frontend-webapp/src/app/vendor-dashboard/reservation/vendor-reservation.component.spec.ts
@@ -4,7 +4,7 @@ import { of as observableOf } from 'rxjs/observable/of';
 import { VendorReservationComponent } from './vendor-reservation.component';
 import { VendorReservationService } from './vendor-reservation.service';
 
-fdescribe('VendorReservationComponent', () => {
+describe('VendorReservationComponent', () => {
   let component: VendorReservationComponent;
   let fixture: ComponentFixture<VendorReservationComponent>;
 
