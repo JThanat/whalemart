0eb5a11d8b62655dc7476739f3f70a0d4eef7a38
Kasidit Iamthong <kasidit@outlook.com>
2017-11-28 08:02:38 +0700

    Add unapproved markets information

--- frontend-webapp/src/app/lessor-dashboard/manage-market/lessor-market-resolver.service.ts
+++ frontend-webapp/src/app/lessor-dashboard/manage-market/lessor-market-resolver.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { combineLatest as observableCombineLatest } from 'rxjs/observable/combineLatest';
+import { map } from 'rxjs/operators';
+
 import { LessorService, MarketList } from '../../core/lessor/lessor.service';
+import { Market } from '../../core/market/market.service';
 
 @Injectable()
 export class LessorMarketResolverService {
   constructor(private lessorService: LessorService) {}
 
-  resolve(): Observable<MarketList> {
-    return this.lessorService.getMarketList();
+  resolve(): Observable<{ unapproved: Market[]; all: MarketList }> {
+    return observableCombineLatest(
+      this.lessorService.getUnapprovedMarketList(),
+      this.lessorService.getMarketList()
+    ).pipe(map(([unapproved, all]) => ({ unapproved, all })));
   }
 }
